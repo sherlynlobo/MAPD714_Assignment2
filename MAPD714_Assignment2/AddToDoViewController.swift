071b39310520f6844977804b5478c41754db8d00
sherlynlobo <sherlynlobo26@gmail.com>
2018-12-08 00:34:30 -0500

    Made connections to addToDoViewController

---
+++ MAPD714_Assignment2/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  MAPD714_Assignment2
+//
+//  Created by Sherlyn Lobo on 2018-12-08.
+//  Copyright © 2018 Sherlyn Lobo. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleTextField: UILabel!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func addTapped(_ sender: UIButton) {
+    }
+    
+
+   
+
+}
